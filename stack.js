766363aec3459abd1064b526bbce8bb5888e5583
Cyprien Pannier <cyprien.pannier@gmail.com>
2021-05-04 21:39:26 +0200

    Initial commit

---
+++ stack.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const cdk = require('@aws-cdk/core');
+
+class EcsServiceStack extends cdk.Stack {
+  /**
+   *
+   * @param {cdk.Construct} scope
+   * @param {string} id
+   * @param {cdk.StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
+
+const app = new cdk.App();
+new EcsServiceStack(app, 'stack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
